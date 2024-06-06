592b6023532807e5f4209d5b0ca71d79de74d561
Lupo Solitario <duyluong1994@gmail.com>
2024-06-07 00:24:08 +0700

    Updates

--- _app/immutable/chunks/paths.Cy0cXJKS.js
+++
@@ -1,1 +0,0 @@
-var s;const e=((s=globalThis.__sveltekit_1y3lhg4)==null?void 0:s.base)??"/alienw_wallet";var a;const l=((a=globalThis.__sveltekit_1y3lhg4)==null?void 0:a.assets)??e;export{l as a,e as b};
